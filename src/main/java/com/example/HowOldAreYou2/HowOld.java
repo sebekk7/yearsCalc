12aa247a9916fb10ed0ea17b8cda6400ca620ec1
Sebastian <sebekk7@gmail.com>
2020-12-12 22:13:35 +0100

    refactor handleNewOrder

--- src/main/java/com/example/HowOldAreYou2/HowOld.java
+++ src/main/java/com/example/HowOldAreYou2/HowOld.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class HowOld {
+
     private int yearOfBirth;
-    private LocalDate localDate;
+
 }
--- src/main/java/com/example/HowOldAreYou2/HowOldController.java
+++ src/main/java/com/example/HowOldAreYou2/HowOldController.java
@@ -1,24 +1,31 @@
 package com.example.HowOldAreYou2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
 @Controller
 public class HowOldController {
 
-    HowOldRepository howOldRepository = new HowOldRepository();
 
     @GetMapping("/form")
-    public String showOrderForm(final ModelMap modelMap){
+    public String showOrderForm(final ModelMap modelMap) {
         modelMap.addAttribute("howold", new HowOld());
         return "form";
     }
 
     @PostMapping("/form")
-    public String handleNewOrder(@ModelAttribute("howold") final HowOld howOld){
+    public String handleNewOrder(@ModelAttribute("howold") final HowOld howOld, Model model) {
+
+        int localDate = LocalDate.now().getYear();
+        String years = String.valueOf(localDate - howOld.getYearOfBirth());
+        model.addAttribute("yearsCalc", years);
+
         return "yearspage";
     }
 
