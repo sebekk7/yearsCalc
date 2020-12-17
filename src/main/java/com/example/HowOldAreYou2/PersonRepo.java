b42d81f4dbf4df87f7ee9b6bbde5aea3deff21c4
Sebastian <sebekk7@gmail.com>
2020-12-17 18:12:56 +0100

    [add] db connection with h2

--- src/main/java/com/example/HowOldAreYou2/PersonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.HowOldAreYou2;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface PersonRepo extends CrudRepository<Person, Long> {
-
-}
--- src/main/java/com/example/HowOldAreYou2/entities/Person.java
+++ src/main/java/com/example/HowOldAreYou2/entities/Person.java
@@ -1,35 +1,27 @@
-package com.example.HowOldAreYou2;
+package com.example.HowOldAreYou2.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 
-@Component
-@Entity
-@Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Person {
 
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
 
     private String name;
+
     private int yearOfBirth;
 
-   // int sum = LocalDate.now().getYear() - yearOfBirth;
-
-    public Person(String name, int yearOfBirth) {
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-    }
-
+    private int years;
 }
