99d19f871394e8bed4e627b58d143cff94abae30
Sebastian <sebekk7@gmail.com>
2020-12-19 19:14:14 +0100

    [add] add/delete to db

--- src/main/java/com/example/HowOldAreYou2/controllers/PersonController.java
+++ src/main/java/com/example/HowOldAreYou2/controllers/PersonController.java
@@ -2,13 +2,13 @@ package com.example.HowOldAreYou2.controllers;
 
 import com.example.HowOldAreYou2.entities.Person;
 import com.example.HowOldAreYou2.repositories.PersonRepo;
+import com.example.HowOldAreYou2.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -31,20 +31,38 @@ public class PersonController {
     public String showOrderForm(final Model model) {
         model.addAttribute("newPerson", new Person());
         model.addAttribute("personList", personList);
+        model.addAttribute("showRepo", personRepo.findAll());
+
         return "form";
     }
 
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        Person person = personRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        personRepo.delete(person);
+        model.addAttribute("users", personRepo.findAll());
+        return "redirect:/form";
+    }
+
+
+
+
+
+
+
+
+
+
+
     @PostMapping("/form")
     public String addUser(Person person, Model model) {
-
         person.setYears(LocalDate.now().getYear() - person.getYearOfBirth());
-
         personRepo.save(person);
-        model.addAttribute("newPerson", personRepo.findAll());
-
         personList.add(person);
         return "redirect:/form";
     }
+
 
 
 }
